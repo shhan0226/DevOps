f67d52287f4ebce47dfb4d365ebddce9e5d8f97d
Ubuntu <shhan0226@gamil.com>
2023-06-18 06:26:32 +0000

    update 2023.06.18

--- install_release/5_ku-clustering-init.sh
+++ install_release/5_ku-clustering-init.sh
@@ -8,8 +8,10 @@ sudo systemctl restart containerd
 # 클러스터 초기화
 # Flannel = 10.244.0.0/16
 # Calico = 192.168.0.0/16
+ip_=$(hostname -I | awk '{print $1}')
+echo $ip_
 sudo kubeadm init --pod-network-cidr=192.168.0.0/16 \
- --control-plane-endpoint="172.16.10.27" --upload-certs
+ --control-plane-endpoint=$ip_ --upload-certs
 
 # set
 mkdir -p $HOME/.kube
@@ -19,5 +21,5 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 # root user 일 경우
 # export KUBECONFIG=/etc/kubernetes/admin.conf
 
-kubectl get pods --all-namespaces
-kubectl get nodes -o wide
+#kubectl get pods --all-namespaces
+#kubectl get nodes -o wide
