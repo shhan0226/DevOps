c793274bc2879f6b949f4cee1621832fdb7fa67e
Ubuntu <shhan0226@gamil.com>
2023-06-18 07:39:16 +0000

    update 2023.06.18

--- kustomize/3_kustomize_install_arm.sh
+++ kustomize/3_kustomize_install_arm.sh
@@ -5,3 +5,4 @@ wget https://raw.githubusercontent.com/kubernetes-sigs/kustomize/kustomize/${KUS
 sed -i "s/arm64)/aarch64)/" install_kustomize.sh
 source install_kustomize.sh
 sudo install -o root -g root -m 0755 kustomize /usr/local/bin/kustomize
+sync
