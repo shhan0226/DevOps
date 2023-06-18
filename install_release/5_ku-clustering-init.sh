1d76d780b034b4043dd109de061aa747800ee635
Ubuntu <shhan0226@gamil.com>
2023-06-18 07:36:29 +0000

    update 2023.06.18

--- install_release/5_ku-clustering-init.sh
+++ install_release/5_ku-clustering-init.sh
@@ -20,5 +20,13 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 # root user 일 경우
 # export KUBECONFIG=/etc/kubernetes/admin.conf
 
+
+# calico 설치
+#curl https://docs.projectcalico.org/manifests/canal.yaml -O
+#kubectl apply -f canal.yaml
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.0/manifests/calico.yaml -O
+kubectl apply -f calico.yaml
+
+
 kubectl get pods --all-namespaces
 kubectl get nodes -o wide
