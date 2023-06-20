c1d9d6561362fc37c6f44764d97523d7bf5191e9
shhan <shhan0226@gmail.com>
2023-06-20 12:14:30 +0900

    update 2023.06.20

--- installer/release_230620/5_ku-clustering-cni.sh
+++ installer/release_230620/5_ku-clustering-cni.sh
@@ -9,6 +9,5 @@
 curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.0/manifests/calico.yaml -O
 kubectl apply -f calico.yaml
 
-
 kubectl get pods --all-namespaces
 kubectl get nodes -o wide
