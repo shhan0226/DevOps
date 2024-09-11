ae169e645a12df71ee40b3d4a234d80617ee0dcf
shhan <shhan@xslab.co.kr>
2024-09-11 15:47:21 +0900

    update 2024.09.11

--- kuberentes/cluster_init.sh
+++ kuberentes/cluster_init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # root 관리자인지 확인
-if [[ $EUID -ne 0 ]]; then
-    echo "it's not root ..."
-    exit 1
-fi
+# if [[ $EUID -ne 0 ]]; then
+#     echo "it's not root ..."
+#     exit 1
+# fi
 
 # 시스템의 IP 주소를 가져옴
 IP_ADDRESSES=$(hostname -I)
@@ -18,7 +18,7 @@ echo $MASTER_IP_1
 CNI_IP="10.244.0.0/16"
 
 # init 명령어
-kubeadm init --apiserver-advertise-address=${MASTER_IP_1} --pod-network-cidr=${CNI_IP} --control-plane-endpoint=${MASTER_IP_1} --upload-certs
+sudo kubeadm init --apiserver-advertise-address=${MASTER_IP_1} --pod-network-cidr=${CNI_IP} --control-plane-endpoint=${MASTER_IP_1} --upload-certs
 
 # 디록토리 설정
 mkdir -p $HOME/.kube
@@ -26,10 +26,10 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 # 홈 디렉토리 설정
-echo "================"
-echo "home user set..."
-HOMEUSER=$(ls /home/.)
-echo $HOMEUSER
-mkdir -p /home/$HOMEUSER/.kube
-sudo cp -i /etc/kubernetes/admin.conf /home/$HOMEUSER/.kube/config
-sudo chown $HOMEUSER:$HOMEUSER /home/$HOMEUSER/.kube/config
+# echo "================"
+# echo "home user set..."
+# HOMEUSER=$(ls /home/.)
+# echo $HOMEUSER
+# mkdir -p /home/$HOMEUSER/.kube
+# sudo cp -i /etc/kubernetes/admin.conf /home/$HOMEUSER/.kube/config
+# sudo chown $HOMEUSER:$HOMEUSER /home/$HOMEUSER/.kube/config
