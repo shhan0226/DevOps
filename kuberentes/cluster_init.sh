103061a2698cf1952cbd7614cd5e25c6cc6d5dc2
shhan <shhan@xslab.co.kr>
2024-09-11 12:06:06 +0900

    update 2024.09.11

---
+++ kuberentes/cluster_init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# root 관리자인지 확인
+if [[ $EUID -ne 0 ]]; then
+    echo "it's not root ..."
+    exit 1
+fi
+
+# 시스템의 IP 주소를 가져옴
+IP_ADDRESSES=$(hostname -I)
+
+# 첫 번째 IP 주소만 추출
+MASTER_IP_1=$(echo $IP_ADDRESSES | awk '{print $1}')
+
+# CNI 주소 
+CNI_IP="10.244.0.0/16"
+
+# init 명령어
+kubeadm init --apiserver-advertise-address=${MASTER_IP_1} --pod-network-cidr=${CNI_IP} --control-plane-endpoint=${MASTER_IP_1} --upload-certs
