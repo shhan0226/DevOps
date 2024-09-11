2f813ec074470865a95ee3ac6b90129a252edbc9
shhan <shhan@xslab.co.kr>
2024-09-11 10:29:04 +0900

    update 2024.09.11

--- env_config/hostname_set.sh
+++ env_config/hostname_set.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# 현재 사용자가 root인지 확인
-# if [ "$EUID" -ne 0 ]; then
-#   echo "Please run as root"
-#   exit 1
-# fi
-
 # hostname 설정
 hostname1=master1
 echo ${hostname1}
--- env_config/hosts_set.sh
+++ env_config/hosts_set.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# 현재 사용자가 root인지 확인
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit 1
+fi
+
 # hosts 설정
 HOSTNAME1=master1
 SERVER_IP=$(hostname -I)
