bb8ae3af2c4351eb33a8fece50e2f42d73275a4f
shhan <shhan@xslab.co.kr>
2024-09-11 11:36:02 +0900

    update 2024.09.11

--- Kuberentes/install_kubernetes.sh
+++ Kuberentes/install_kubernetes.sh
@@ -26,32 +26,28 @@ sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
 
 # 키 및 저장소 등록
-# ver.1
 # If the folder `/etc/apt/keyrings` does not exist, it should be created before the curl command, read the note below.
-# sudo mkdir -p -m 755 /etc/apt/keyrings
-# curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-# sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
+sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
 
-# ver.2
 # This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list   # helps tools such as command-not-found to work correctly
 
 # kubelet, kubeadm, kubectl 설치
 sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
+sudo apt-get install -y kubelet kubeadm kubectl bash-completion
+sudo apt-mark hold kubelet kubeadm kubectl bash-completion
 
 # (옵션) error 처리
+sudo rm /etc/containerd/config.toml
 sudo mkdir -p /etc/containerd
 containerd config default | sudo tee /etc/containerd/config.toml
 sudo sed -i 's/ SystemdCgroup = fasle/ SystemdCgroup = true/' /etc/containerd/config.toml
-# sudo rm /etc/containerd/config.toml
-# sudo systemctl restart containerd
+sudo systemctl restart containerd
 
-# auto 설정
-kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl
-source /etc/bash_completion.d/kubectl
+# # auto 설정
+# kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl
+# source /etc/bash_completion.d/kubectl
 
-# kubeadm version
-kubectl version --short
